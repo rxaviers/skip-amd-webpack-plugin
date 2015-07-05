32cd83b3092449d91a5b39847ca6195d53a2287e
Rafael Xavier de Souza <rxaviers@gmail.com>
2015-07-05 12:01:19 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+function SkipAMDOfUMDPlugin(requestRegExp) {
+  this.requestRegExp = requestRegExp;
+}
+
+SkipAMDOfUMDPlugin.prototype.apply = function(compiler) {
+  var requestRegExp = this.requestRegExp;
+
+  compiler.parser.plugin("can-rename define", function() {
+    return false;
+  });
+  compiler.parser.plugin("evaluate typeof define", function(expr) {
+    if (requestRegExp.test(this.state.current.request)) {
+      var res = compiler.parser.evaluate("typeof (false)");
+      res.setRange(expr.range);
+      return res;
+    }
+  });
+  compiler.parser.plugin("typeof define", function(expr) {
+    if (requestRegExp.test(this.state.current.request)) {
+      var dep = new ConstDependency("typeof (false)", expr.range);
+      dep.loc = expr.loc;
+      this.state.current.addDependency(dep);
+      return true;
+    }
+  });
+
+};
+
+module.exports = SkipAMDOfUMDPlugin;

886c383c1c6f5f828b50a868399d0b0700680b31
Rafael Xavier de Souza <rxaviers@gmail.com>
2015-08-28 19:02:30 -0300

    Allow to update the regular expression on the fly

--- index.js
+++ index.js
@@ -5,20 +5,29 @@ function SkipAMDPlugin(requestRegExp) {
 }
 
 SkipAMDPlugin.prototype.apply = function(compiler) {
-  var requestRegExp = this.requestRegExp;
+  // Note: We're using `self.requestRegExp` instead of a plain `requestRegExp`
+  // variable to allow this variable to be changed dynamically. Because, it
+  // allows the following hacky usage. In other words, it allows one to update
+  // the regular expression on the fly instead of appending more plugins.
+  //
+  // https://github.com/rxaviers/globalize-webpack-plugin/blob/v0.1.1/ProductionModePlugin.js#L78-L92
+  //
+  // This hacky usage shouldn't be needed if Tapable provides a way to prepend
+  // plugins instead of just appending them.
+  var self = this;
 
   compiler.parser.plugin("can-rename define", function() {
     return false;
   });
   compiler.parser.plugin("evaluate typeof define", function(expr) {
-    if (requestRegExp.test(this.state.current.request)) {
+    if (self.requestRegExp.test(this.state.current.request)) {
       var res = compiler.parser.evaluate("typeof (false)");
       res.setRange(expr.range);
       return res;
     }
   });
   compiler.parser.plugin("typeof define", function(expr) {
-    if (requestRegExp.test(this.state.current.request)) {
+    if (self.requestRegExp.test(this.state.current.request)) {
       var dep = new ConstDependency("typeof (false)", expr.range);
       dep.loc = expr.loc;
       this.state.current.addDependency(dep);
